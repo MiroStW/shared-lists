5a40303330bb1ca474c692e55c554bd6ee8e61aa
Miro <miro@miro-wilms.de>
2022-08-02 21:08:57 +0200

    added lists styles

--- components/lists/List.tsx
+++ components/lists/List.tsx
@@ -1,9 +1,10 @@
 import { List as ListType } from "../../types/types";
+import styles from "../../styles/list.module.css";
 
 const List = ({ list }: { list: ListType }) => {
   return (
-    <div className="list">
-      <div className="listTitle">{list.data.name}</div>
+    <div className={styles.list}>
+      <div className={styles.listTitle}>{list.data.name}</div>
     </div>
   );
 };
--- components/lists/Lists.tsx
+++ components/lists/Lists.tsx
@@ -1,15 +1,17 @@
+import { useEffect } from "react";
 import { collection, orderBy, query, where } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { useEffect } from "react";
-import { auth, db } from "../../firebase/firebase";
+import { User } from "firebase/auth";
+import { db } from "../../firebase/firebase";
 import List from "./List";
 import { listConverter } from "../../firebase/firestoreConverter";
+import styles from "../../styles/lists.module.css";
 
-const Lists = () => {
+const Lists = ({ user }: { user: User }) => {
   const [lists, loading, error] = useCollection(
     query(
       collection(db, "lists"),
-      where("ownerID", "==", auth.currentUser?.uid),
+      where("ownerID", "==", user?.uid),
       where("isArchived", "==", false)
       // orderBy("createdDate", "desc")
     ).withConverter(listConverter),
@@ -21,11 +23,11 @@ const Lists = () => {
   }, [lists]);
 
   return (
-    <div className="listsArea">
+    <div className={styles.listsArea}>
       <div className="listsHeader">
         <h2>Lists</h2>
       </div>
-      <div className="listList">
+      <div className={styles.listList}>
         {lists?.docs.map((list) => {
           return <List key={list.id} list={list.data()} />;
         })}
