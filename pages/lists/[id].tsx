98a0e5dbaec5d44e740c42c5770e0fa5f21eb8b7
Miro <miro@miro-wilms.de>
2022-08-05 16:30:54 +0200

    fixed id router query

--- pages/lists/[id].tsx
+++ pages/lists/[id].tsx
@@ -1,25 +1,40 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/header/Header";
 import { Items } from "../../components/items/Items";
 import { Lists } from "../../components/lists/Lists";
+import { Loading } from "../../components/Loading";
 import { useLists } from "../../firebase/listsContext";
 import styles from "../../styles/showApp.module.css";
+import { List } from "../../types/types";
 
-const ShowApp = ({ id }: { id: string }) => {
-  const { lists } = useLists();
+const ShowApp = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [list, setList] = useState<List | undefined>(undefined);
+  const { lists, loading: loadingLists } = useLists();
 
-  const list = lists?.find((el) => id === el.ref.id);
+  useEffect(() => {
+    console.log(id);
+    if (!loadingLists && lists) setList(lists?.find((el) => id === el.ref.id));
+  }, [id, lists]);
 
-  // const [list, loading, error, reload] = useDocumentOnce(
-  //   doc(db, "lists/ReVsf81YvgD7w0HnOVwz").withConverter(listConverter)
-  // );
+  if (loadingLists) return <Loading />;
 
   return (
-    <div>
+    <div id={styles.container}>
       <Header />
       <div id={styles.main}>
-        <Lists />
-        {list && <Items list={list} />}
-        {/* get open list from route */}
+        {loadingLists ? (
+          <Loading />
+        ) : (
+          <>
+            <Lists />
+            <div style={{ position: "relative", flex: 1 }}>
+              {list ? <Items list={list} /> : <Loading />}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
