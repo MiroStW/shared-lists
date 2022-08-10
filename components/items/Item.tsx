fb606044e9fc23429f82e77fdd9431e0d375d8b1
Miro <miro@miro-wilms.de>
2022-08-10 11:56:51 +0200

    added delete item button

--- components/items/Item.tsx
+++ components/items/Item.tsx
@@ -1,12 +1,23 @@
+import { deleteDoc } from "firebase/firestore";
 import styles from "../../styles/item.module.css";
 import { Item as ItemType } from "../../types/types";
+import { Icon } from "../utils/Icon";
 import { Checkbox } from "./Checkbox";
 
 const Item = ({ item }: { item: ItemType }) => {
+  const handleDelete = () => {
+    deleteDoc(item.ref);
+  };
+
   return (
     <div className={`${styles.item} ${item.data.completed && styles.complete}`}>
       <Checkbox item={item} />
       <div className={styles.itemName}>{item.data.name}</div>
+      <div className={styles.itemHoverMenu}>
+        <div className={styles.deleteButton} onClick={handleDelete}>
+          <Icon iconName="delete" />
+        </div>
+      </div>
     </div>
   );
 };
