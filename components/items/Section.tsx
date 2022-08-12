a4941e002f722ed42f8d888f370ba4e9e67420c1
Miro <miro@miro-wilms.de>
2022-08-12 14:06:20 +0200

    section open/close function

--- components/items/Section.tsx
+++ components/items/Section.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import styles from "../../styles/section.module.css";
 import { Section as SectionType } from "../../types/types";
 import { Icon } from "../utils/Icon";
 import { Items } from "./Items";
 
 const Section = ({ section }: { section: SectionType }) => {
+  const [isOpen, setIsOpen] = useState(true);
   const handleDelete = () => {
     console.log("click");
   };
 
   return (
-    <div className={`${styles.section} `}>
+    <div className={styles.section}>
       <div className={styles.sectionHeader}>
+        <div
+          className={styles.sectionToggle}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <Icon iconName={isOpen ? "unfold_less" : "unfold_more"} size={24} />
+        </div>
         <div className={styles.sectionName}>{section.data.name}</div>
         <div className={styles.sectionHoverMenu}>
           <div className={styles.deleteButton} onClick={handleDelete}>
