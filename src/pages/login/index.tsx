aed7d8ed992273da1ebc76152b7ec652fb05ccf2
Miro <miro@miro-wilms.de>
2022-08-19 20:58:05 +0200

    push to /lists after login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import router from "next/router";
+import { Loading } from "../../components/utils/Loading";
+import { Error } from "../../components/utils/Error";
+import { uiConfig } from "../../firebase/firebaseAuthUI.config";
+import { useAuth } from "../../firebase/authContext";
+
+const Login = () => {
+  const { user, loading, error, auth } = useAuth();
+  // const router = useRouter();
+
+  if (loading) return <Loading />;
+  else if (error) return <Error msg={error.message} />;
+  else if (user) {
+    // user is already logged in, redirect to home page
+    router.push("/lists");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>sharedLists | LogIn</title>
+      </Head>
+      <div>
+        <div>
+          <h1>Log in</h1>
+        </div>
+        <StyledFirebaseAuth uiConfig={uiConfig()} firebaseAuth={auth} />
+      </div>
+    </>
+  );
+};
+
+export default Login;
