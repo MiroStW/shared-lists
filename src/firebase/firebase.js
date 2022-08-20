8fb04d0bd216b7e4d0bd0508e0d66d7476896c3b
Miro <miro@miro-wilms.de>
2022-08-20 21:34:41 +0200

    enable local functions emulator

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,8 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 
 import { firebaseConfig } from "./firebase-config";
 
 const firebase = initializeApp(firebaseConfig);
+
+const functions = getFunctions(firebase);
+connectFunctionsEmulator(functions, "localhost", 5001);
 
 export { firebase };
