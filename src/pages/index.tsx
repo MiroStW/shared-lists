5226febad543de5176984a808d05871f9f756f4f
Miro <miro@miro-wilms.de>
2022-08-21 20:58:41 +0200

    limited loader to loginStatus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,32 +3,34 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { Loading } from "../components/utils/Loading";
 import { useAuth } from "../firebase/authContext";
+import styles from "../styles/main.module.css";
 
 const Home: NextPage = () => {
   const { user, loading, auth } = useAuth();
-
-  if (loading) return <Loading />;
 
   return (
     <>
       <h1>Shared Lists</h1>
       <p>This is an empty home page, to be filled.</p>
-      <p>
-        {user ? (
+      <div className={styles.loginStatus}>
+        {loading && <Loading size={40} />}
+
+        {user && !loading && (
           <>
             <Link href="/lists">
               <button>open app</button>
             </Link>
             <button onClick={() => signOut(auth)}>logout</button>
           </>
-        ) : (
+        )}
+        {!user && !loading && (
           <>
             <button>
               <Link href={"/login"}>Login</Link>
             </button>
           </>
         )}
-      </p>
+      </div>
     </>
   );
 };
