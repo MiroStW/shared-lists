a3a5d2810807614d8abbaeb235071af1e9e21172
Miro <miro@miro-wilms.de>
2022-08-24 21:02:03 +0200

    adding list error handling

--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -1,15 +1,17 @@
 import List from "./List";
 import styles from "../../styles/lists.module.css";
 import { useLists } from "../../firebase/listsContext";
+import { Error } from "../utils/Error";
 
 const Lists = () => {
-  const { lists } = useLists();
+  const { lists, error } = useLists();
 
   return (
     <div className={styles.listsArea}>
       <div className="listsHeader">
         <h2>Lists</h2>
       </div>
+      {error && <Error msg={error.message} />}
       <div className={styles.listList}>
         {lists?.map((list) => {
           return <List key={list.ref.id} list={list} />;
