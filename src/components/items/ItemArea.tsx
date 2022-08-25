ab54d0e1f13cad6c787f574e467de34b45443383
Miro Wilms <miro@miro-wilms.de>
2022-08-25 13:32:59 +0200

    temp commit

--- src/components/items/ItemArea.tsx
+++ src/components/items/ItemArea.tsx
@@ -1,10 +1,10 @@
-import { DragEndEvent } from "@dnd-kit/core";
+import { DragEndEvent, DndContext } from "@dnd-kit/core";
 import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { addDoc, deleteDoc } from "firebase/firestore";
-import { itemsOfList, itemsOfSection } from "../../firebase/useDb";
+import { items, itemsOfList, itemsOfSection } from "../../firebase/useDb";
 import styles from "../../styles/items.module.css";
 import { Item, List, Section } from "../../types/types";
 import { Droppable } from "../utils/Droppable";
@@ -12,6 +12,9 @@ import { Items } from "./Items";
 import { Sections } from "./Sections";
 
 const ItemArea = ({ list }: { list: List }) => {
+  // query all items of a list in object {root: [item1, item2, item3], section1:
+  // [item1, item2], section2: [item1, item2]}
+
   const handleDragEnd = (e: DragEndEvent) => {
     if (e.over) {
       const item = e.active.data.current?.item as Item;
@@ -38,22 +41,19 @@ const ItemArea = ({ list }: { list: List }) => {
 
   return (
     <div className={styles.itemsArea}>
-      {/* <DndContext onDragEnd={(e) => handleDragEnd(e)}> */}
-      {/* <Droppable area={list} type="list-header"> */}
-      <div className={styles.itemsHeader}>
-        <h2>{list.data.name}</h2>
-      </div>
-      {/* </Droppable> */}
-
-      <div className={styles.itemsList}>
-        {/* <Droppable area={list} type="list"> */}
-        <Items parent={list} />
-        {/* </Droppable> */}
-        <Sections list={list} />
-      </div>
-      {/* </DndContext> */}
+      <DndContext onDragEnd={(e) => handleDragEnd(e)}>
+        <div className={styles.itemsHeader}>
+          <h2>{list.data.name}</h2>
+        </div>
+        <div className={styles.itemsList}>
+          {items.map((area) => {
+            if(area !== "root") <div>{area.key}</div>
+          <Items items={area} />
+          })}
+          <Sections list={list} />{" "}
+        </div>{" "}
+      </DndContext>{" "}
     </div>
   );
 };
-
 export { ItemArea };
