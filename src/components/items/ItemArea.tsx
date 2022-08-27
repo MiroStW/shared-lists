7279db8c83af0f57a166eceb7183de3e61064d83
Miro <miro@miro-wilms.de>
2022-08-27 21:42:54 +0200

    moved data fetching to itemArea

--- src/components/items/ItemArea.tsx
+++ src/components/items/ItemArea.tsx
@@ -1,33 +1,45 @@
-import { DragEndEvent, DndContext } from "@dnd-kit/core";
 import {
   DndContext,
   DragEndEvent,
   DragOverlay,
   DragStartEvent,
-  UniqueIdentifier,
 } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+import { orderBy, query, updateDoc, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { useAuth } from "../../firebase/authContext";
 import {
-  arrayMove,
-  SortableContext,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
-import { addDoc, deleteDoc } from "firebase/firestore";
-import { useState } from "react";
-import { items, itemsOfList, itemsOfSection } from "../../firebase/useDb";
+  itemConverter,
+  sectionConverter,
+} from "../../firebase/firestoreConverter";
+import { items as itemsCollection, sectionsOfList } from "../../firebase/useDb";
 import styles from "../../styles/items.module.css";
-import { Item as ItemType, List, Section } from "../../types/types";
-import { Droppable } from "../utils/Droppable";
+import { Item as ItemType, List } from "../../types/types";
+import { Loading } from "../utils/Loading";
+import { Error } from "../utils/Error";
 import { Item } from "./Item";
 import { Items } from "./Items";
-import { Sections } from "./Sections";
 
 const ItemArea = ({ list }: { list: List }) => {
-  const [activeItem, setActiveItem] = useState<ItemType | null>(null);
-  // query all items of a list in object {root: [item1, item2, item3], section1:
-  // [item1, item2], section2: [item1, item2]}
+  const { user } = useAuth();
+  const [items, loading, error] = useCollection(
+    query(
+      itemsCollection,
+      where("list", "==", list.ref.id),
+      where("ownerID", "==", user?.uid),
+      orderBy("order", "asc")
+    ).withConverter(itemConverter)
+  );
+  const [sections, loadingSections, errorSections] = useCollection(
+    query(
+      sectionsOfList(list),
+      where("ownerID", "==", user?.uid)
+    ).withConverter(sectionConverter)
+  );
+  const [activeItem, setActiveItem] = useState<ItemType | null>();
 
   const handleDragEnd = (e: DragEndEvent) => {
-
     setActiveItem(null);
     const { active, over } = e;
 
@@ -66,19 +78,52 @@ const ItemArea = ({ list }: { list: List }) => {
 
   return (
     <div className={styles.itemsArea}>
-      <DndContext onDragStart={(e) => handleDragStart(e)} onDragEnd={(e) => handleDragEnd(e)}>
+      <DndContext
+        onDragStart={(e) => handleDragStart(e)}
+        onDragEnd={(e) => handleDragEnd(e)}
+      >
         <div className={styles.itemsHeader}>
           <h2>{list.data.name}</h2>
         </div>
         <div className={styles.itemsList}>
-          {items.map((area) => {
-            if(area !== "root") <div>{area.key}</div>
-          <Items items={area} />
-          })}
-          <Sections list={list} />
+          {error && <Error msg={error.message} />}
+          {errorSections && <Error msg={errorSections.message} />}
+          {loading || loadingSections ? (
+            <Loading />
+          ) : (
+            <>
+              {items && (
+                <>
+                  <Items
+                    id={list.ref.id}
+                    items={items.docs
+                      .filter(
+                        (item) => item.ref.parent.parent?.id === list.ref.id
+                      )
+                      .map((item) => item.data())}
+                  />
+                  {sections?.docs
+                    .map((sectionObj) => sectionObj.data())
+                    .map((section) => (
+                      <div key={section.ref.id}>
+                        <div>{section.data.name}</div>
+                        <Items
+                          id={section.ref.id}
+                          items={items.docs
+                            .filter(
+                              (item) =>
+                                item.ref.parent.parent?.id === section.ref.id
+                            )
+                            .map((item) => item.data())}
+                        />
+                      </div>
+                    ))}
+                </>
+              )}
+            </>
+          )}
         </div>
       </DndContext>
-
       <DragOverlay>
         {activeItem ? <Item item={activeItem} /> : null}
       </DragOverlay>
--- src/components/items/Items.tsx
+++ src/components/items/Items.tsx
@@ -2,49 +2,25 @@ import {
   SortableContext,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { orderBy, query, where } from "firebase/firestore";
-import { useCollection } from "react-firebase-hooks/firestore";
-import { useAuth } from "../../firebase/authContext";
-import { itemConverter } from "../../firebase/firestoreConverter";
-import { itemsOfList, itemsOfSection } from "../../firebase/useDb";
-import { List, Section } from "../../types/types";
-import { Error } from "../utils/Error";
-import { Loading } from "../utils/Loading";
+import { useDroppable } from "@dnd-kit/core";
 import { Item } from "./Item";
+import { Item as ItemType } from "../../types/types";
 
-const Items = ({ parent }: { parent: List | Section }) => {
-  const { user } = useAuth();
-  const [items, loading, error] = useCollection(
-    query(
-      parent.ref.parent.id === "lists"
-        ? itemsOfList(parent as List)
-        : itemsOfSection(parent),
-      where("ownerID", "==", user?.uid),
-      orderBy("order", "asc")
-    ).withConverter(itemConverter),
-    { snapshotListenOptions: { includeMetadataChanges: true } }
-  );
-
+const Items = ({ items, id }: { items: ItemType[]; id: string }) => {
+  const { setNodeRef } = useDroppable({ id });
   return (
     <>
-      {error && <Error msg={error.message} />}
-      {loading ? (
-        <Loading />
-      ) : (
-        items && (
-          <>
-            <SortableContext
-              items={items.docs.map((item) => item.ref.id)}
-              strategy={verticalListSortingStrategy}
-            >
-              {items.docs.map((item) => (
-                <Item key={item.ref.id} item={item.data()} />
-              ))}
-            </SortableContext>
-          </>
-        )
-        // </Droppable>
-      )}
+      <SortableContext
+        id={id}
+        items={items.map((item) => item.ref.id)}
+        strategy={verticalListSortingStrategy}
+      >
+        <div ref={setNodeRef}>
+          {items.map((item) => (
+            <Item key={item.ref.id} item={item} />
+          ))}
+        </div>
+      </SortableContext>
     </>
   );
 };
