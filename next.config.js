2ce56503f33456be43bb54770462ea27bfd0d518
Miro <miro@miro-wilms.de>
2022-09-07 21:40:26 +0300

    enabled react.StrictMode

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     FIREBASE_PROJECT_ID: "shared-lists-8fc29",
   },
   distDir: "build",
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: [
