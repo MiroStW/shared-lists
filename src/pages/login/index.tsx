c81757ba24791df9ac7db8338e32482fbdf04368
Miro <miro@miro-wilms.de>
2022-09-07 21:40:01 +0300

    removed react-firebaseui dependency

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import router from "next/router";
 import { Loading } from "../../components/utils/Loading";
 import { Error } from "../../components/utils/Error";
 import { uiConfig } from "../../firebase/firebaseAuthUI.config";
 import { useAuth } from "../../firebase/authContext";
+import StyledFirebaseUi from "../../firebase/StyledFirebaseUi";
 
 const Login = () => {
   const { user, loading, error, auth } = useAuth();
@@ -26,7 +26,7 @@ const Login = () => {
         <div>
           <h1>Log in</h1>
         </div>
-        <StyledFirebaseAuth uiConfig={uiConfig()} firebaseAuth={auth} />
+        <StyledFirebaseUi uiConfig={uiConfig()} firebaseAuth={auth} />
       </div>
     </>
   );
