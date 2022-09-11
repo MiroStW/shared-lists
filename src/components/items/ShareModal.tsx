4896d2c4b2dec05b6d9c5fc4e6ffa92d9042b6e0
Miro <miro@miro-wilms.de>
2022-09-11 13:19:24 +0300

    added form to Sharemodal

--- src/components/items/ShareModal.tsx
+++ src/components/items/ShareModal.tsx
@@ -1,5 +1,5 @@
 import { addDoc } from "firebase/firestore";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { useAuth } from "../../firebase/authContext";
 import { createInviteData } from "../../firebase/factory";
 import { invites } from "../../firebase/useDb";
@@ -14,13 +14,32 @@ const ShareModal = ({
   setShowShareModal: Dispatch<SetStateAction<boolean>>;
 }) => {
   const { user } = useAuth();
-  const handleShare = (inviteeEmail: string) => {
-    if (user) addDoc(invites, createInviteData(user, inviteeEmail, list));
+  const [email, setEmail] = useState("");
+  const handleShare = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+    e.preventDefault();
+    if (user && email) {
+      addDoc(invites, createInviteData(user, email, list));
+      setShowShareModal(false);
+    }
   };
 
   return (
-    <Modal setOpenModal={setShowShareModal}>
-      <>test</>
+    <Modal
+      setOpenModal={setShowShareModal}
+      title={`Who do you want to share ${list.data.name} with?`}
+    >
+      {/* TODO: add validation */}
+      <form>
+        <label htmlFor="inviteeEmail">E-Mail:</label>
+        <input
+          autoFocus
+          type="text"
+          name="inviteeEmail"
+          placeholder="share@with.me"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input type="submit" value="Submit" onClick={handleShare} />
+      </form>
     </Modal>
   );
 };
