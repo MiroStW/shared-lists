c19e28a7037980a1865225555df78993d976ee47
Miro <miro@miro-wilms.de>
2022-10-16 20:19:56 +0200

    ssr list attempt

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -1,3 +1,6 @@
+import { getAuth } from "firebase/auth";
+import { getDocs, orderBy, query, where } from "firebase/firestore";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { AddButton } from "../../components/addButton/AddButton";
@@ -5,42 +8,75 @@ import { Header } from "../../components/header/Header";
 import { ItemAreaContainer } from "../../components/items/ItemAreaContainer";
 import { Lists } from "../../components/lists/Lists";
 import { Loading } from "../../components/utils/Loading";
+import { firebase } from "../../firebase/firebase";
+import { listConverter } from "../../firebase/firestoreConverter";
 import { useLists } from "../../firebase/listsContext";
+import { lists as listsRef } from "../../firebase/useDb";
 import styles from "../../styles/showApp.module.css";
 import { List } from "../../types/types";
 import { RouteProps } from "../_app";
 
-const ShowApp = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [list, setList] = useState<List | undefined>(undefined);
-  const { lists, loading: loadingLists } = useLists();
+const ShowApp = () =>
+  // { ssrLists }: { ssrLists: List[] }
+  {
+    const router = useRouter();
+    const { id } = router.query;
+    const { lists, loading } = useLists();
+    const [list, setList] = useState<List | undefined>(undefined);
 
-  useEffect(() => {
-    if (!loadingLists && lists) setList(lists?.find((el) => id === el.ref.id));
-  }, [id, lists, loadingLists]);
+    // useEffect(() => {
+    //   if (ssrLists) console.log("ssrLists: ", ssrLists);
+    // }, [ssrLists]);
 
-  return (
-    <div id={styles.container}>
-      <Header />
-      <div id={styles.main}>
-        {loadingLists ? (
-          <Loading />
-        ) : (
-          <>
-            <Lists />
-            {list ? <ItemAreaContainer list={list} /> : <Loading />}
-            {list && <AddButton activeList={list} />}
-          </>
-        )}
+    useEffect(() => {
+      if (!loading && lists) setList(lists?.find((el) => id === el.ref.id));
+    }, [id, lists, loading]);
+
+    return (
+      <div id={styles.container}>
+        <Header />
+        <div id={styles.main}>
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              <Lists />
+              {list ? <ItemAreaContainer list={list} /> : <Loading />}
+              {list && <AddButton activeList={list} />}
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const user = getAuth(firebase).currentUser;
+
+  // const listsSnapshot = await getDocs(
+  //   query(
+  //     listsRef,
+  //     where("ownerID", "==", user?.uid),
+  //     where("isArchived", "==", false),
+  //     orderBy("createdDate", "asc")
+  //   ).withConverter(listConverter)
+  // );
+
+  // const ssrLists = listsSnapshot.docs.map((doc) => doc.data());
+
+  return {
+    props: {
+      protectedRoute: true,
+      // ssrLists,
+    },
+  };
 };
 
-export const getServerSideProps = async (): Promise<{ props: RouteProps }> => {
+// works like SSR on intiial load, then loads statically generated page
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    props: { protectedRoute: true },
+    paths: [],
+    fallback: "blocking",
   };
 };
 
