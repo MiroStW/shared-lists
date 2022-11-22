afa23bea5d72cb16f9ac9e4e04ba9f87135572e6
Miro <miro@miro-wilms.de>
2022-11-22 11:57:25 +0100

    initial working token-based auth

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,15 +1,11 @@
-import { getApp } from "firebase/app";
-import {
-  initializeApp as initializeAdminApp,
-  credential,
-} from "firebase-admin";
-import { getApps as getAdminApps } from "firebase-admin/app";
+import * as admin from "firebase-admin";
+import { getApps, getApp } from "firebase-admin/app";
 
 import { firebaseAdminConfig } from "./firebase-config";
 
-const firebaseAdmin = !getAdminApps().length
-  ? initializeAdminApp({
-      credential: credential.cert({
+const firebaseAdmin = !getApps().length
+  ? admin.initializeApp({
+      credential: admin.credential.cert({
         projectId: firebaseAdminConfig.project_id,
         clientEmail: firebaseAdminConfig.client_email,
         privateKey: firebaseAdminConfig.private_key,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,80 @@
 // eslint-disable-next-line import/no-unresolved
 import { getAuth } from "firebase-admin/auth";
-import { Auth, signOut, User } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import type {
-  GetServerSideProps,
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
-  NextPage,
 } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import nookies from "nookies";
-import { Loading } from "../components/utils/Loading";
 import { useAuth } from "../firebase/authContext";
 import { firebaseAdmin } from "../firebase/firebaseAdmin";
 import styles from "../styles/main.module.css";
 
-interface SSRProps {
-  auth: Auth;
-}
-
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   try {
     const cookies = nookies.get(ctx);
-    const token = await getAuth(firebaseAdmin).verifyIdToken(cookies.token);
+    if (cookies.token) {
+      const token = await getAuth(firebaseAdmin).verifyIdToken(cookies.token);
+      const { uid } = token;
+      const user = await getAuth(firebaseAdmin).getUser(uid);
+      const serializedUser = JSON.stringify(user);
 
-    const { uid } = token;
-
-    const user = await getAuth(firebaseAdmin).getUser(uid);
+      return {
+        props: { serializedUser },
+      };
+    } else return { props: {} as never };
+  } catch (error) {
+    // ctx.res.writeHead(302, { Location: "/login" });
+    // ctx.res.end();
+    const serializedError = JSON.stringify(error);
 
     return {
-      props: { user },
-    };
-  } catch (err) {
-    ctx.res.writeHead(302, { Location: "/login" });
-    ctx.res.end();
-    console.log("err: ", err);
-    return {
-      props: {} as never,
+      props: {
+        serializedUser: null,
+        serializedError,
+      },
     };
   }
 };
 
 const Home = ({
-  user,
+  serializedUser,
+  serializedError,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const user = serializedUser ? JSON.parse(serializedUser) : null;
+  const error = serializedError ? JSON.parse(serializedError) : null;
   const { auth } = useAuth();
+  const router = useRouter();
   // const { user, loading, auth } = useAuth();
+
   return (
     <>
       <h1>Shared Lists</h1>
       <p>This is an empty home page, to be filled.</p>
       <div className={styles.loginStatus}>
         {/* {loading && <Loading size={40} />} */}
-
+        {error && (
+          <div>
+            <p>There was an error:</p>
+            {error.message}
+            {/* {Object.keys(error).forEach((key) => (
+              <p>
+                {key}: {error[key]}
+              </p>
+            ))} */}
+          </div>
+        )}
         {user && (
           // !loading &&
           <>
             <Link href="/lists">
               <button>open app</button>
             </Link>
-            <button onClick={() => signOut(auth)}>logout</button>
+            <button onClick={() => signOut(auth).then(() => router.push("/"))}>
+              logout
+            </button>
           </>
         )}
         {!user && (
