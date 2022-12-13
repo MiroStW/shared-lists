5183496d012b70f2faff95a41fc284967f6db86c
Miro <miro@miro-wilms.de>
2022-12-13 21:14:13 +0100

    added debug messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,54 @@ import type {
 } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { useAuth } from "../firebase/authContext";
 import { verifyAuthToken } from "../firebase/verifyAuthToken";
 import styles from "../styles/main.module.css";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const { user } = await verifyAuthToken(ctx);
+  const { user, fromVerifyAuthToken } = await verifyAuthToken(ctx);
 
   return {
     props: user
       ? {
           serializedUser: JSON.stringify(user),
+          fromServer:
+            "this means getServerSideProps code is executed & user found",
+          fromVerifyAuthToken,
+          ctx: JSON.stringify(ctx.req.headers.cookie),
         }
-      : ({} as never),
+      : {
+          fromServer:
+            "this means getServerSideProps code is executed & user not found",
+          fromVerifyAuthToken,
+          ctx: JSON.stringify(ctx.req.headers.cookie),
+        },
   };
 };
 
+// PRODUCTION returned user seems to be missing, /lists pushes me back to
+// /login, on index I see login button, but token is still there in local storage
+
 const Home = ({
   serializedUser,
+  fromServer,
+  fromVerifyAuthToken,
+  ctx,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const user = serializedUser ? (JSON.parse(serializedUser) as User) : null;
   const { auth } = useAuth();
   const router = useRouter();
   // const { user, loading, auth } = useAuth();
+
+  useEffect(() => {
+    console.log("serializedUser: ", serializedUser);
+    console.log("user: ", user);
+    console.log("auth: ", auth);
+    console.log("fromServer: ", fromServer);
+    console.log("fromVerifyAuthToken: ", fromVerifyAuthToken);
+    console.log("ctx: ", ctx);
+  }, [auth, ctx, fromServer, fromVerifyAuthToken, serializedUser, user]);
 
   return (
     <>
