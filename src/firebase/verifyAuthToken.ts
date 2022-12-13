cd1a8f5efe8c23d4159f5fb67d29edad4f5cf44d
Miro <miro@miro-wilms.de>
2022-12-13 13:28:45 +0100

    added auth return & unserialized user

--- src/firebase/verifyAuthToken.ts
+++ src/firebase/verifyAuthToken.ts
@@ -4,27 +4,25 @@ import { firebaseAdmin } from "./firebaseAdmin";
 import nookies from "nookies";
 
 const verifyAuthToken = async (ctx: GetServerSidePropsContext) => {
+  const auth = getAuth(firebaseAdmin);
   try {
     const cookies = nookies.get(ctx);
     if (cookies.token) {
+      console.log("found token");
       const token = await getAuth(firebaseAdmin).verifyIdToken(cookies.token);
       const { uid } = token;
-      const user = await getAuth(firebaseAdmin).getUser(uid);
-      const serializedUser = JSON.stringify(user);
-
+      console.log("uid found: ", uid);
+      const user = await auth.getUser(uid);
       return {
-        serializedUser,
+        auth,
+        user,
       };
     }
   } catch (error) {
-    // DOESN'T WORK YET? not supposed to happen on "/" route,maybe add parameter to redirect to login page
-    // if (ctx.req.url !== "/login") {
-    //   ctx.res.writeHead(302, { Location: "/login" });
-    //   ctx.res.end();
-    // }
-    return {};
+    console.log("error", error);
+    return { auth };
   }
-  return {};
+  return { auth };
 };
 
 export { verifyAuthToken };
