55eec3861ce7208bddd83b4fe580d138a79fa64c
Miro <miro@miro-wilms.de>
2022-12-13 13:31:45 +0100

    prefetched lists

--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -2,8 +2,10 @@ import List from "./List";
 import styles from "../../styles/lists.module.css";
 import { useLists } from "../../firebase/listsContext";
 import { Error } from "../utils/Error";
+import { AdminList } from "../../types/types";
+import { Loading } from "../utils/Loading";
 
-const Lists = () => {
+const Lists = ({ preFetchedLists }: { preFetchedLists?: AdminList[] }) => {
   const { lists, error } = useLists();
 
   return (
@@ -13,9 +15,20 @@ const Lists = () => {
       </div>
       {error && <Error msg={error.message} />}
       <div className={styles.listList}>
-        {lists?.map((list) => {
-          return <List key={list.ref.id} list={list} />;
-        })}
+        {
+          // render prefetched lists on initial load
+          preFetchedLists && lists?.length === 0
+            ? preFetchedLists.map((list) => (
+                <List key={`pfl${list.ref.id}`} list={list} />
+              ))
+            : null
+        }
+        {
+          // listen to lists changes after intial load
+          lists &&
+            lists.map((list) => <List key={`crl${list.ref.id}`} list={list} />)
+        }
+        {lists?.length === 0 && preFetchedLists?.length === 0 && <Loading />}
       </div>
     </div>
   );
