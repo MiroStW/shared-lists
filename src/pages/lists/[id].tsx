6ec5888817762bbdce92fde6ab4e48129e3080a1
Miro <miro@miro-wilms.de>
2022-12-13 13:32:06 +0100

    prefetched lists

--- src/pages/lists/[id].tsx
+++ src/pages/lists/[id].tsx
@@ -1,4 +1,4 @@
-import { GetServerSidePropsContext } from "next";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { AddButton } from "../../components/addButton/AddButton";
@@ -6,21 +6,68 @@ import { Header } from "../../components/header/Header";
 import { ItemAreaContainer } from "../../components/items/ItemAreaContainer";
 import { Lists } from "../../components/lists/Lists";
 import { Loading } from "../../components/utils/Loading";
-import { useLists } from "../../firebase/listsContext";
+import { useAuth } from "../../firebase/authContext";
+import { adminDb } from "../../firebase/firebaseAdmin";
 import { verifyAuthToken } from "../../firebase/verifyAuthToken";
 import styles from "../../styles/showApp.module.css";
-import { List } from "../../types/types";
+import { AdminList } from "../../types/types";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const { serializedUser } = await verifyAuthToken(ctx);
+  const { user } = await verifyAuthToken(ctx);
 
-  if (serializedUser)
+  if (user) {
+    const ownedLists = adminDb()
+      .collection("lists")
+      .where("ownerID", "==", user.uid)
+      .where("isArchived", "==", false)
+      .orderBy("createdDate", "asc")
+      // .withConverter(listConverter)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          return [];
+        }
+        return snapshot.docs.map(
+          (doc) =>
+            ({
+              data: doc.data(),
+              ref: { ...doc.ref, id: doc.id },
+            } as AdminList)
+        );
+        // QUESTION: is there a better way to add getter functions like doc.id
+        // to the ref? Without this, the getter functions are stripped away by JSON.stringify
+      });
+
+    const joinedLists = adminDb()
+      .collection("lists")
+      .where("contributors", "array-contains", user.uid)
+      .where("isArchived", "==", false)
+      .orderBy("createdDate", "asc")
+      // .withConverter(listConverter)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          return [];
+        }
+        return snapshot.docs.map(
+          (doc) =>
+            ({
+              data: doc.data(),
+              ref: { ...doc.ref, id: doc.id },
+            } as AdminList)
+        );
+      });
+
+    const lists = await Promise.all([ownedLists, joinedLists]).then((values) =>
+      values.flat()
+    );
+
     return {
       props: {
-        serializedUser,
+        serializedLists: JSON.stringify(lists),
       },
     };
-  else
+  } else
     return {
       redirect: {
         permanent: false,
@@ -30,38 +77,50 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     };
 };
 
-const ShowApp = () =>
-  // { ssrLists }: { ssrLists: List[] }
-  {
-    const router = useRouter();
-    const { id } = router.query;
-    const { lists, loading } = useLists();
-    const [list, setList] = useState<List | undefined>(undefined);
+const ShowList = ({
+  serializedLists,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { user } = useAuth();
+  const [lists, setLists] = useState<AdminList[] | null>(null);
+  const [activeList, setActiveList] = useState<AdminList | undefined>(
+    undefined
+  );
 
-    // useEffect(() => {
-    //   if (ssrLists) console.log("ssrLists: ", ssrLists);
-    // }, [ssrLists]);
+  useEffect(() => {
+    if (serializedLists) {
+      const preFetchedLists = JSON.parse(serializedLists) as AdminList[];
+      const foundActiveList = preFetchedLists.find(
+        (list) => list.ref.id === id
+      );
+      if (!foundActiveList) {
+        router.push("/lists");
+      } else {
+        setActiveList(foundActiveList);
+      }
+      setLists(preFetchedLists);
+    }
+  }, [id, router, serializedLists]);
 
-    useEffect(() => {
-      if (!loading && lists) setList(lists?.find((el) => id === el.ref.id));
-    }, [id, lists, loading]);
-
-    return (
-      <div id={styles.container}>
-        <Header />
-        <div id={styles.main}>
-          {loading ? (
-            <Loading />
-          ) : (
+  return (
+    <div id={styles.container}>
+      <Header />
+      <div id={styles.main}>
+        <Lists preFetchedLists={lists || undefined} />
+        <div className={styles.itemsArea}>
+          {activeList && user ? (
             <>
-              <Lists />
-              {list ? <ItemAreaContainer list={list} /> : <Loading />}
-              {list && <AddButton activeList={list} />}
+              <ItemAreaContainer list={activeList} />
+              <AddButton activeList={activeList} />
             </>
+          ) : (
+            <Loading />
           )}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default ShowApp;
+export default ShowList;
