bee13e9cae99eebbaaf013d429a8dddae4a25fc6
Miro <miro@miro-wilms.de>
2023-01-05 17:20:49 +0100

    added title to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Cookie } from "next-cookie";
 import AuthContextProvider from "../firebase/authContext";
 import { ListsContextProvider } from "../firebase/listsContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider cookie={new Cookie()}>
-      <ListsContextProvider>
-        <Component {...pageProps} />
-      </ListsContextProvider>
-    </AuthContextProvider>
+    <>
+      <Head>
+        <title>Shared Lists</title>
+        <meta property="og:title" content="Shared Lists" key="title" />
+      </Head>
+      <AuthContextProvider cookie={new Cookie()}>
+        <ListsContextProvider>
+          <Component {...pageProps} />
+        </ListsContextProvider>
+      </AuthContextProvider>
+    </>
   );
 }
 
