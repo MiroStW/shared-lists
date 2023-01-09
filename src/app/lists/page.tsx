ebdc28ef8b396fffbb28f729e50129f25a9775f1
Miro <miro@miro-wilms.de>
2023-01-09 10:58:26 +0100

    redirect to 1st list, else to /login

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,15 +1,33 @@
-export const getServerSideProps = async () => {
-  return {
-    redirect: {
-      permanent: false,
-      destination: "/login",
-    },
-    props: {} as never,
-  };
+import { redirect } from "next/navigation";
+import { createAdminListData } from "../../firebase/adminFactory";
+import { adminDb } from "../../firebase/firebaseAdmin";
+import { verifyAuthToken } from "../context/verifyAuthToken";
+
+const getFirstListId = async () => {
+  const { user } = await verifyAuthToken();
+  if (user) {
+    const firstListId = await adminDb()
+      .collection("lists")
+      .where("ownerID", "==", user.uid)
+      .where("isArchived", "==", false)
+      .orderBy("createdDate", "asc")
+      .limit(1)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          return null;
+        }
+        return snapshot.docs[0].id;
+      });
+    return firstListId;
+  }
+  return null;
 };
 
-const Lists = () => {
-  return null;
+const Lists = async () => {
+  const firstListId = await getFirstListId();
+  if (firstListId) redirect(`/lists/${firstListId}`);
+  redirect("/login");
 };
 
 export default Lists;
