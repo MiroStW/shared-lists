4e8a6af0eae411f55eeb4fe77ae96b35ceb40669
Miro <miro@miro-wilms.de>
2023-01-10 22:16:38 +0100

    fixed adminDb import

--- src/app/(app)/getLists.tsx
+++ src/app/(app)/getLists.tsx
@@ -1,6 +1,6 @@
 import { verifyAuthToken } from "app/verifyAuthToken";
-import { adminDb } from "firebase/firebaseAdmin";
 import { AdminList } from "types/types";
+import { adminDb } from "../../firebase/firebaseAdmin";
 
 export const getLists = async () => {
   const { user } = await verifyAuthToken();
