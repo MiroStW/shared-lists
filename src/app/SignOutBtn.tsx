1037523fe926e49191dd4f01ebfaf1a4fb48ea81
miro.wilms <miro.wilms@aoe.com>
2023-10-09 22:15:19 +0200

    feat(firebase-auth): revoke server auth session on signout

--- src/app/SignOutBtn.tsx
+++ src/app/SignOutBtn.tsx
@@ -3,11 +3,21 @@
 import { signOut } from "firebase/auth";
 import { useClientSession } from "./sessionContext";
 
-
 const SignOutBtn = () => {
   const { auth } = useClientSession();
 
-  return <button onClick={() => signOut(auth)}>sign out</button>
+  const signOutHandler = async () => {
+    signOut(auth);
+    try {
+      const data = await fetch("/api/revokesession");
+      const res = data.json();
+      console.log("revoked session", res);
+    } catch (err) {
+      console.error("fetch error: ", err);
+    }
+  };
+
+  return <button onClick={signOutHandler}>sign out</button>;
 };
 
 export default SignOutBtn;
