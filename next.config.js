606d8446337710cc42ef92a3c5061b3addedae54
miro.wilms <miro.wilms@aoe.com>
2023-10-16 09:55:46 +0200

    fix(firebase-auth): add coop header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/signin",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
   env: {
     FIREBASE_PROJECT_ID: "shared-lists-8fc29",
   },
